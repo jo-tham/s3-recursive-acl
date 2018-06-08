9564e6d8f6e7cf8e10b8912c0992327c87e8b11c
Jotham Apaloo <jotham.apaloo@ospreyinformatics.com>
2018-06-08 13:27:52 -0700

    latest

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"sync"
 	"log"
 
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
@@ -28,47 +29,73 @@ func Handler(ctx context.Context, e events.DynamoDBEvent) {
 		// stdout and stderr are sent to AWS CloudWatch Logs
 		log.Printf("Processing %s %s %s %s\n", region, bucket, path, acl)
 
-		// var wg sync.WaitGroup
-		// var counter int64
-		// async := nasync.New(1000,1000)
-		// defer async.Close()
+		var wg sync.WaitGroup
+		var counter int64
+		async := nasync.New(1000,1000)
+		defer async.Close()
 
-		// svc := s3.New(session.New(), &aws.Config{
-		// 	Region: aws.String(region),
-		// })
+		svc := s3.New(session.New(), &aws.Config{
+			Region: aws.String(region),
+		})
+		var hasError bool
 
-		// err := svc.ListObjectsPages(&s3.ListObjectsInput{
-		// 	Prefix: aws.String(path),
-		// 	Bucket: aws.String(bucket),
-		// }, func(page *s3.ListObjectsOutput, lastPage bool) bool {
-		// 	for _, object := range page.Contents {
-		// 		key := *object.Key
-		// 		counter++
-		// 		async.Do(func(bucket string, key string, cannedACL string) {
-		// 			wg.Add(1)
-		// 			_, err := svc.PutObjectAcl(&s3.PutObjectAclInput{
-		// 				ACL:    aws.String(cannedACL),
-		// 				Bucket: aws.String(bucket),
-		// 				Key:    aws.String(key),
-		// 			})
-		// 			log.Printf(fmt.Sprintf("Updating '%s'", key))
-		// 			if err != nil {
-		// 				log.Printf(fmt.Sprintf("Failed to change permissions on '%s', %v", key, err))
-		// 			}
-		// 			defer wg.Done()
-		// 		}, bucket, key, acl)
-		// 	}
-		// 	return true
-		// })
+		err := svc.ListObjectsPages(&s3.ListObjectsInput{
+			Prefix: aws.String(path),
+			Bucket: aws.String(bucket),
+		}, func(page *s3.ListObjectsOutput, lastPage bool) bool {
+			for _, object := range page.Contents {
+				key := *object.Key
+				counter++
+				async.Do(func(bucket string, key string, cannedACL string) {
+					wg.Add(1)
+					_, err := svc.PutObjectAcl(&s3.PutObjectAclInput{
+						ACL:    aws.String(cannedACL),
+						Bucket: aws.String(bucket),
+						Key:    aws.String(key),
+					})
+					log.Printf(fmt.Sprintf("Updating '%s'", key))
+					if err != nil {
+						log.Printf(fmt.Sprintf("Failed to change permissions on '%s', %v", key, err))
+						hasError = true
+						log.Print(hasError)
+					}
+					defer wg.Done()
+				}, bucket, key, acl)
+			}
+			return true
+		})
 
-		// wg.Wait()
+		wg.Wait()
+		if err != nil {
+			log.Printf(fmt.Sprintf("Failed to update object permissions in '%s', %v", bucket, err))
+			panic(fmt.Sprintf("Failed to update object permissions in '%s', %v", bucket, err))
+		}
+		log.Printf(fmt.Sprintf("Successfully updated permissions on %d objects", counter))
 
-		// if err != nil {
-		// 	log.Printf(fmt.Sprintf("Failed to update object permissions in '%s', %v", bucket, err))
-		// 	panic(fmt.Sprintf("Failed to update object permissions in '%s', %v", bucket, err))
-		// }
+		if hasError {
+			log.Printf("There was an error updating objects at %s, not deleting dynamodb item", path)
+			return
+		}
+		sess, _ := session.NewSession( &aws.Config{
+			Region: aws.String("us-west-2")},
+		)
+		dbsvc := dynamodb.New(sess)
+		input := &dynamodb.DeleteItemInput{
+			Key: map[string]*dynamodb.AttributeValue{
+				"path": {
+					S: aws.String(path),
+				},
+			},
+			TableName: aws.String("set-s3-objects-acl"),
+		}
+		_, err = dbsvc.DeleteItem(input)
+		if err != nil {
+			log.Printf("Got error calling DeleteItem")
+			log.Printf(err.Error())
+			return
+		}
+		log.Printf("Deleted dynamodb record for %s %s %s", region, bucket, path)
 
-		// log.Printf(fmt.Sprintf("Successfully updated permissions on %d objects", counter))
 	}
 }
 
