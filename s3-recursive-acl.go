18985e20fea95640f0f86e6314bdc9d4b920be81
Jotham Apaloo <jotham.apaloo@ospreyinformatics.com>
2018-06-08 12:04:53 -0700

    make lambda

--- s3-recursive-acl.go
+++ s3-recursive-acl.go
@@ -1,62 +1,78 @@
 package main
 
 import (
-	"flag"
+	"context"
 	"fmt"
-	"os"
 	"sync"
+	"log"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/ti/nasync"
 )
 
-func main() {
-	var bucket, region, path, cannedACL string
-	var wg sync.WaitGroup
-	var counter int64
-	async := nasync.New(1000,1000)
-	defer async.Close()
-	flag.StringVar(&region, "region", "ap-northeast-1", "AWS region")
-	flag.StringVar(&bucket, "bucket", "s3-bucket", "Bucket name")
-	flag.StringVar(&path, "path", "", "Path to recurse under")
-	flag.StringVar(&cannedACL, "acl", "public-read", "Canned ACL to assign objects")
-	flag.Parse()
+// Handler is your Lambda function handler
+func Handler(ctx context.Context, e events.DynamoDBEvent) {
+	for _, record := range e.Records {
+		log.Printf("Processing request data for event ID %s, type %s.\n", record.EventID, record.EventName)
 
-	svc := s3.New(session.New(), &aws.Config{
-		Region: aws.String(region),
-	})
+		// Print new values for attributes name and age
+		acl := record.Change.NewImage["acl"].String()
+		bucket := record.Change.NewImage["bucket"].String()
+		region := record.Change.NewImage["region"].String()
+		path := record.Change.NewImage["path"].String()
 
-	err := svc.ListObjectsPages(&s3.ListObjectsInput{
-		Prefix: aws.String(path),
-		Bucket: aws.String(bucket),
-	}, func(page *s3.ListObjectsOutput, lastPage bool) bool {
-		for _, object := range page.Contents {
-			key := *object.Key
-			counter++
-			async.Do(func(bucket string, key string, cannedACL string) {
-				wg.Add(1)
-				_, err := svc.PutObjectAcl(&s3.PutObjectAclInput{
-					ACL:    aws.String(cannedACL),
-					Bucket: aws.String(bucket),
-					Key:    aws.String(key),
-				})
-				fmt.Println(fmt.Sprintf("Updating '%s'", key))
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "Failed to change permissions on '%s', %v", key, err)
-				}
-				defer wg.Done()
-			}, bucket, key, cannedACL)
-		}
-		return true
-	})
+		// stdout and stderr are sent to AWS CloudWatch Logs
+		log.Printf("Processing %s %s %s %s\n", region, bucket, path, acl)
 
-	wg.Wait()
+		// var wg sync.WaitGroup
+		// var counter int64
+		// async := nasync.New(1000,1000)
+		// defer async.Close()
 
-	if err != nil {
-		panic(fmt.Sprintf("Failed to update object permissions in '%s', %v", bucket, err))
+		// svc := s3.New(session.New(), &aws.Config{
+		// 	Region: aws.String(region),
+		// })
+
+		// err := svc.ListObjectsPages(&s3.ListObjectsInput{
+		// 	Prefix: aws.String(path),
+		// 	Bucket: aws.String(bucket),
+		// }, func(page *s3.ListObjectsOutput, lastPage bool) bool {
+		// 	for _, object := range page.Contents {
+		// 		key := *object.Key
+		// 		counter++
+		// 		async.Do(func(bucket string, key string, cannedACL string) {
+		// 			wg.Add(1)
+		// 			_, err := svc.PutObjectAcl(&s3.PutObjectAclInput{
+		// 				ACL:    aws.String(cannedACL),
+		// 				Bucket: aws.String(bucket),
+		// 				Key:    aws.String(key),
+		// 			})
+		// 			log.Printf(fmt.Sprintf("Updating '%s'", key))
+		// 			if err != nil {
+		// 				log.Printf(fmt.Sprintf("Failed to change permissions on '%s', %v", key, err))
+		// 			}
+		// 			defer wg.Done()
+		// 		}, bucket, key, acl)
+		// 	}
+		// 	return true
+		// })
+
+		// wg.Wait()
+
+		// if err != nil {
+		// 	log.Printf(fmt.Sprintf("Failed to update object permissions in '%s', %v", bucket, err))
+		// 	panic(fmt.Sprintf("Failed to update object permissions in '%s', %v", bucket, err))
+		// }
+
+		// log.Printf(fmt.Sprintf("Successfully updated permissions on %d objects", counter))
 	}
-
-	fmt.Println(fmt.Sprintf("Successfully updated permissions on %d objects", counter))
 }
+
+func main() {
+	lambda.Start(Handler)
+}
+
